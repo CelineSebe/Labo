67d337ddf80ed68daf523e23acca6fc21f4d141f
Céline Sèbe <celine.sebe@gmail.com>
2022-10-31 16:52:51 +0100

    menu

--- gestion_labo.py
+++ gestion_labo.py
@@ -4,12 +4,24 @@ from labo_cmd import *
 def main():
     quitter = False
     while not quitter:
+        # afficher le menu
         print("1- Enregistrer une nouvelle personne")
         print("2- Supprimer les infos d'une personne")
         print("3- Modifier les infos d'une personne")
-        print(f'4- {quitter}')
+        print("4- Quitter")
 
-        # afficher le menu
         # demander le choix
+        choice = int(input("Votre choix : "))
+
         # traiter le choix
+        if choice == 1:
+            ajouter()
+        elif choice == 2:
+            supprimer()
+        elif choice == 3:
+            modifier()
+        else:
+            quitter = True
+
+
 main()
